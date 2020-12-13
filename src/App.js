5d4de323f1e3f6967f208408fb17fe3811645149
Cyrus Hanson <cyrus.hanson@gmail.com>
2020-12-13 12:09:08 -0600

    Restructured files to seperate components and pages. Debugged errors

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import NavTabs from "./components/NavTabs";
-import Home from "./components/pages/Home";
-import Login from "./components/pages/Login";
-import Chat from "./components/pages/Chat";
-import Reserve from "./components/pages/Reserve";
+import InfoBar from "./components/InfoBar/InfoBar";
+import Home from "./pages/Home";
+// import Login from "./components/pages/Login";
+import Chat from "./pages/Chat";
+import Reserve from "./pages/Reserve";
+// import Message from "./components/Messages/Message";
+// import Messages from "./components/Messages";
+// import TextContainer from "./components/TextContainer";
+
+
+
+
+
 import './App.css';
 
 function App() {
   return (
     <Router>
-    <div>
-      <NavTabs />
-      <Route exact path="/" component={Home} />
-      <Route exact path="/login" component={Login} />
-      <Route exact path="/chat" component={Chat} />
-      <Route path="/reserve" component={Reserve} />
-    </div>
-  </Router>
+      <div>
+        <NavTabs />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route path="/reserve" component={Reserve} />
+        <Route path='/' exact component={Join} />
+        <Route path='/chat' component={Chat} />
+      </div>
+    </Router>
   );
 }
+
+
+
 
 export default App;
